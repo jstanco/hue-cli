33b5d95b6985726e6bc230e34b3913e950b35342
John Stanco <stancoj9@gmail.com>
2021-08-22 01:55:41 -0400

    Remove unnecessary imports

--- hue/descriptor.py
+++ hue/descriptor.py
@@ -1,5 +1,5 @@
 from functools import partial
-from typing import Any, Callable, Dict, Optional, Type, TypeVar, Union
+from typing import Any, Callable, Dict, Optional, Type, TypeVar
 
 from hue.controllable import Controllable
 
